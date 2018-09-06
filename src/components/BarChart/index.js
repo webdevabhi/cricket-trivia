5373aba1f6ad3bc4a7774b83edba6617942938b9
webdevabhi <abhishek.patel185@gmail.com>
2018-09-06 14:39:15 +0530

    Chart Integrated

--- src/components/BarChart/index.js
+++ src/components/BarChart/index.js
@@ -1,9 +1,33 @@
 import React from 'react';
 
+import { Bar } from 'react-chartjs-2';
+
 const barChart = () => {
+    const bar = {
+        labels: ['Correct', 'In Correct'],
+        datasets: [
+            {
+                label: 'Result',
+                backgroundColor: 'rgba(255,99,132,0.2)',
+                borderColor: 'rgba(255,99,132,1)',
+                borderWidth: 2,
+                hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+                hoverBorderColor: 'rgba(255,99,132,1)',
+                data: [3, 7],
+            },
+        ],
+    };
+
+    const options = {
+        tooltips: {
+            enabled: false,
+        },
+        maintainAspectRatio: false
+    }
+
     return (
         <div>
-            <p>This is Bar chart component</p>
+            <Bar data={bar} options={options} />
         </div>
     );
 };
