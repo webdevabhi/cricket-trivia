64369d87884e117bcbd579760a9523e85015a7f4
webdevabhi <abhishek.patel185@gmail.com>
2018-09-06 14:27:13 +0530

    Redux to home connection

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react';
+import { connect } from "react-redux";
+import { fetchQuestions } from "../../store/actions/index";
 
 class Home extends Component {
+    componentDidMount() {
+        this.props.fetchQuestions();
+    }
+
     render() {
+        let form = <li>No Questions</li>;
+        if(this.props.questions && this.props.questions.length) {
+            console.log(this.props.questions);
+        }
         return (
             <div>
-                <p>Home Component</p>
+                <p>Hi User, I am home Component</p>
+                <ul>
+                    {form}
+                </ul>
             </div>
         );
     }
 }
 
-export default Home;
+const mapStateToProps = (state) => {
+    return { 
+        questions: state.quizData.questions
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        fetchQuestions: () => dispatch(fetchQuestions())
+    };
+};
+
+export default  connect(mapStateToProps, mapDispatchToProps)(Home);
