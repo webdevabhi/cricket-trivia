029060cc18fb164350016d4492098c09d3df8350
webdevabhi <abhishek.patel185@gmail.com>
2018-09-06 14:20:01 +0530

    Added Layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Layout from "./Layout";
 
 class App extends Component {
   render() {
     return (
       <div>
-        React App
+        <Layout />
       </div>
     );
   }
---
+++ src/Layout/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from "../components/Header";
+import Home from "../pages/Home";
+
+const layout = () => {
+    return (
+        <div>
+            <Header />
+            <Home />
+        </div>
+    );
+};
+
+export default layout;
