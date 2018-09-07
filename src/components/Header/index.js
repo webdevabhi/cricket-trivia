ef0b5db240fbec27d71573d2a4f43322841453da
webdevabhi <abhishek.patel185@gmail.com>
2018-09-07 15:13:17 +0530

    Header a tag href changed to script url

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,10 +5,10 @@ const header = () => {
         <nav className="navbar navbar-inverse">
             <div className="container">
                 <div className="navbar-header">
-                    <a className="navbar-brand" href="/">Cricket Trivia</a>
+                    <a className="navbar-brand" href="javascript:void(0)">Cricket Trivia</a>
                 </div>
                 <ul className="nav navbar-nav">
-                    <li className="active"><a href="/">Home</a></li>
+                    <li className="active"><a href="javascript:void(0)">Home</a></li>
                 </ul>
             </div>
         </nav>
